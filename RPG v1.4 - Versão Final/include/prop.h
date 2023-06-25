b4327a1266800c71d546bec67f5782db4ce1c237
Lucas <128428285+LucasCAlbas@users.noreply.github.com>
2023-06-25 00:20:32 -0300

    Add files via upload

---
+++ RPG v1.4 - Versão Final/include/prop.h
@@ -0,0 +1,12 @@
+#include "raylib.h"
+
+class Prop{
+    public:
+        Prop(Vector2 pos, Texture2D tex);
+        void Render(Vector2 heroPos);
+        Rectangle GetCollisionRec(Vector2 heroPos);
+    private:
+        Texture2D texture{};
+        Vector2 worldPos{};
+        float scale{4.f};
+};
